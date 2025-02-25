91b2dd5a477e42ccff3402669306f85169e70847
Sriram <sriram2005j@gmail.com>
2025-02-25 19:10:44 +0530

    fixed eslint

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,20 +1,22 @@
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successful",
+      success: true,
+    });
 
-export async function GET(){
-    try {
-        const response = NextResponse.json({
-            message: "Logout successful",
-            success: true
-        })
-        
-        response.cookies.set("token", "",
-            {httpOnly:true, expires: new Date(0)}
-        )
-        return response
-    } catch (error) {
-        return NextResponse.json({error: (error as Error).message}),
-        {status: 500}
-        
-    }
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
